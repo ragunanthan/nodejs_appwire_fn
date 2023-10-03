5ab6e53a67f188aada3f41643da427283206691a
Ragunanthan <66114285+ragunanthan@users.noreply.github.com>
2023-10-03 19:47:14 +0530

    sd

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Client } from 'node-appwrite';
+import { Client } from "node-appwrite";
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
@@ -10,25 +10,38 @@ export default async ({ req, res, log, error }) => {
   //    .setKey(process.env.APPWRITE_API_KEY);
 
   // You can log messages to the console
-  log('Hello, Logs!');
+  log("Hello, Logs!");
 
   // If something goes wrong, log an error
-  error('Hello, Errors!');
+  error("Hello, Errors!");
 
   // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    const data = await  fetch("https://rapidapi.com/felixeschmittfes/api/amazonlive", { method : "GET"});
-    console.log(data.body)
-    return res.send("tesrt");
+  if (req.method === "GET") {
+    const url =
+      "https://amazonlive.p.rapidapi.com/product?asin=B095JCHDMV&location=us";
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "cb7fd9219cmsh061b0c7aa0470e0p1995f2jsnf40a3bc0d352",
+        "X-RapidAPI-Host": "amazonlive.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      return res.send(result);
+    } catch (error) {
+      return res.send(error);
+    }
+   
   }
 
   // `res.json()` is a handy helper for sending JSON
   return res.json({
-    motto: 'Build Fast. Scale Big. All in One Place.',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
+    motto: "Build Fast. Scale Big. All in One Place.",
+    learn: "https://appwrite.io/docs",
+    connect: "https://appwrite.io/discord",
+    getInspired: "https://builtwith.appwrite.io",
   });
 };
