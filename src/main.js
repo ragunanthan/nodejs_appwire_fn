23594dd07654da2a99ef3215c8af3401b15039d2
Ragunanthan <66114285+ragunanthan@users.noreply.github.com>
2023-10-04 12:29:14 +0530

    sdfs

--- src/main.js
+++ src/main.js
@@ -10,12 +10,6 @@ const databases = new Databases(client);
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
 
   // You can log messages to the console
   log("Hello, Logs!");
@@ -25,6 +19,7 @@ export default async ({ req, res, log, error }) => {
 
   // The `req` object contains the request data
   if (req.method === "GET") {
+    console.log(req)
     const url =
       "https://amazonlive.p.rapidapi.com/product?asin=B095JCHDMV&location=us";
     const options = {
